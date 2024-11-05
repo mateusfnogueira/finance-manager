ab2020f6b4a0e569f14af31ef68283f0f31c9b91
mateusfnogueira <mateusfnogueira@outlook.com>
2024-11-05 20:38:34 -0300

    chore: install deps and shad config

--- next.config.js
+++ next.config.js
@@ -1,30 +1,32 @@
+const { postcss } = require('tailwindcss')
+
 /** @type {import('next').NextConfig} */
-const nextIntl = require("next-intl/plugin")();
+const nextIntl = require('next-intl/plugin')()
 
 const nextConfig = nextIntl({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: [
         {
-          loader: "@svgr/webpack",
+          loader: '@svgr/webpack',
           options: {
-            icon: true,
-          },
-        },
-      ],
-    });
-    return config;
-  },
-});
+            icon: true
+          }
+        }
+      ]
+    })
+    return config
+  }
+})
 
-module.exports = nextConfig;
+module.exports = nextConfig
