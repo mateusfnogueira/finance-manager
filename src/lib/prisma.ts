f1730cc6b62da21fa1b5cb36ea66863efaad5e79
mateusfnogueira <mateusfnogueira@outlook.com>
2024-11-07 08:06:19 -0300

    chore: add docker compose and refact prisma config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,16 @@
 import { PrismaClient } from '@prisma/client'
+declare global {
+  var cachedPrisma: PrismaClient
+}
+let prisma: PrismaClient
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+if (process.env.NODE_ENV !== 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient()
+  }
+  prisma = global.cachedPrisma
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const db = prisma
