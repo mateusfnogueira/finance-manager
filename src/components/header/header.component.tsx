7e306c1dfa326caa69a233ddc0882f9fd9e0b659
mateusfnogueira <mateusfnogueira@outlook.com>
2024-11-09 13:39:21 -0300

    feat: validate to not render header

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -10,15 +10,48 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import Logo from '@public/icons/logo-main.svg'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export function Header() {
   const { data: session } = useSession()
+  const pathname = usePathname()
+
+  if (!session) {
+    return null
+  }
 
   return (
-    <header className="flex items-center justify-between bg-primary-foreground px-8 py-4">
-      <div className="flex items-center gap-2">
-        <Logo />
-        <h1 className="text-xl font-bold">finance.ai</h1>
+    <header className="border-solidpx-8 flex items-center justify-between border-b bg-primary-foreground px-8 py-4">
+      <div className="flex justify-between gap-3">
+        <div className="flex items-center gap-2">
+          <Logo />
+          <h1 className="text-xl font-bold">finance.ai</h1>
+        </div>
+        <nav className="flex items-center gap-10">
+          <Link
+            href="/"
+            className={pathname === '/' ? 'font-bold text-primary' : 'text-muted-foreground'}
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="/transactions"
+            className={
+              pathname === '/transactions' ? 'font-bold text-primary' : 'text-muted-foreground'
+            }
+          >
+            Transações
+          </Link>
+          <Link
+            href="/subscription"
+            className={
+              pathname === '/subscription' ? 'font-bold text-primary' : 'text-muted-foreground'
+            }
+          >
+            Assinatura
+          </Link>
+        </nav>
       </div>
       {session ? (
         <DropdownMenu>
