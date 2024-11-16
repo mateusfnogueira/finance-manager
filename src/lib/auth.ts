ff8ac1269bf88aba45f23c42a0e612b070a06dbe
Mateus.nogueira-extern <mateus.nogueira-extern@renault.com>
2024-11-16 14:25:35 -0300

    feat: add stripe props to user and create stripe customer when signin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,11 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 import { AuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { db } from './prisma'
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2024-10-28.acacia'
+})
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db),
@@ -13,6 +18,23 @@ export const authOptions: AuthOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
+    async signIn({ user, account, profile }) {
+      let customerList = await stripe.customers.list({ email: user.email as string })
+      let customer = customerList.data.length
+        ? customerList.data[0]
+        : await stripe.customers.create({
+            email: user.email as string
+          })
+
+      await db.user.update({
+        where: { email: user.email as string },
+        data: {
+          stripeCustomerId: customer.id
+        }
+      })
+
+      return true
+    },
     async session({ session, token, user }) {
       if (session && session.user) {
         session.user.id = user.id
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -8,6 +8,7 @@ declare module 'next-auth' {
       email?: string | null
       image?: string | null
       subscriptionPlan?: string | null
+      stripeCustomerId?: string | null
     }
   }
 }
